93af118d61174e0b5de35a3f3de10511663133d7
Yavor Mishev <y.mishev27@gmail.com>
2023-06-28 23:28:42 +0300

    Added task 02.SimpleCalculator

---
+++ Java Advanced/src/_01_JavaAdvanced/_01_StacksAndQueues/Lab/_02_SimpleCalculator.java
@@ -0,0 +1,11 @@
+package _01_JavaAdvanced._01_StacksAndQueues.Lab;
+
+import java.util.Scanner;
+
+public class _02_SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
