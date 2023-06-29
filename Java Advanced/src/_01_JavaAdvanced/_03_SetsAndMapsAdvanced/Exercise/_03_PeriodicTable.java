ad274be70f2a6a2714df64737cd546900081adac
Yavor Mishev <y.mishev27@gmail.com>
2023-06-29 22:05:13 +0300

    Added task 03.PeriodicTable

--- Java Advanced/src/_01_JavaAdvanced/_03_SetsAndMapsAdvanced/Exercise/_03_PeriodicTable.java
+++ Java Advanced/src/_01_JavaAdvanced/_03_SetsAndMapsAdvanced/Exercise/_03_PeriodicTable.java
@@ -1,11 +1,23 @@
 package _01_JavaAdvanced._03_SetsAndMapsAdvanced.Exercise;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.TreeSet;
 
 public class _03_PeriodicTable {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int compoundCount = Integer.parseInt(scanner.nextLine());
 
+        TreeSet<String> uniqueElements = new TreeSet<>();
+
+        for (int i = 0; i < compoundCount; i++) {
+            String[] compound = scanner.nextLine().split(" ");
+            uniqueElements.addAll(Arrays.asList(compound));
+        }
+        for (String elem : uniqueElements) {
+            System.out.print(elem + " ");
+        }
     }
 }
